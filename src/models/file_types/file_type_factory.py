dc929a4ac875909912a84c99407080287f1b1668
ftapiat <francisco.tapia.t@gmail.com>
2023-05-05 18:38:55 -0400

    Fix file_type_factory.py importing wrong class

--- src/models/file_types/file_type_factory.py
+++ src/models/file_types/file_type_factory.py
@@ -1,4 +1,4 @@
-from .file_type_enum import FileTypeEnum
+from .file_type_name import FileTypeName
 from .file_type import FileType
 from .dotenv_file_type import DotenvFileType
 from .oc_yaml_env_obj_file_type import OcYamlEnvObjFileType
@@ -7,12 +7,12 @@ from .oc_yaml_env_list_file_type import OcYamlEnvListFileType
 
 class FileTypeFactory:
     @staticmethod
-    def from_type(type_type: FileTypeEnum, content: str) -> FileType:
-        if type_type == FileTypeEnum.DOTENV:
+    def from_type(type_name: FileTypeName, content: str) -> FileType:
+        if type_name == FileTypeName.DOTENV:
             return DotenvFileType(content)
-        elif type_type == FileTypeEnum.OC_YAML_ENV_OBJ:
+        elif type_name == FileTypeName.OC_YAML_ENV_OBJ:
             return OcYamlEnvObjFileType(content)
-        elif type_type == FileTypeEnum.OC_YAML_ENV_LIST:
+        elif type_name == FileTypeName.OC_YAML_ENV_LIST:
             return OcYamlEnvListFileType(content)
         else:
             raise Exception("Invalid file type.")
