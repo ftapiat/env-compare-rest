eca2b2467c1568ba695ea02be44fda40ca879df2
ftapia <ftapia@losheroes.cl>
2023-06-06 17:40:56 -0400

    Resolve #3

--- src/models/comparable_files/comparable_file.py
+++ src/models/comparable_files/comparable_file.py
@@ -5,10 +5,3 @@ class ComparableFile:
     def __init__(self, name: Optional[str], content: str, default_name: Optional[str] = None):
         self.name = name or default_name
         self.content = content
-
-    @property
-    def serialized(self):
-        return {
-            "name": self.name,
-            "content": self.content
-        }
