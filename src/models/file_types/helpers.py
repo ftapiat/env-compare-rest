360ab9d65ad056f16be184eb364fd729710ee3f9
ftapia <ftapia@losheroes.cl>
2023-06-07 17:14:37 -0400

    Resolve #5

--- src/models/file_types/helpers.py
+++ src/models/file_types/helpers.py
@@ -1,7 +1,10 @@
-def get_list_values_from_yaml_content(yaml_content, object_key: str):
-    list_values = None
+def get_list_values_from_yaml_content(yaml_content, object_key: str, expects_type: type):
     if isinstance(yaml_content, dict) and object_key in yaml_content:
         list_values = yaml_content[object_key]
-    elif isinstance(yaml_content, list):
+    else:
         list_values = yaml_content
-    return list_values
+
+    if expects_type == dict and isinstance(list_values, dict) \
+            or expects_type == list and isinstance(list_values, list):
+        return list_values
+    return None
